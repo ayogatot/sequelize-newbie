cee35677c84ba4b6178d135443e7e035d9178b88
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-20 00:02:31 +0700

    create some data and sync to MySql table

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
 const Sequelize = require("sequelize");
 
 const connection = new Sequelize("demo_database", "root", "password", {
-    dialect: "mysql"
+  dialect: "mysql"
 });
 
 const Author = connection.define("author", {
-    author_id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
+  author_id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
 });
 
 const Book = connection.define("book", {
-    title: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false
-    },
-    author: Sequelize.STRING,
-    author_id: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
+  title: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  author: Sequelize.STRING,
+  author_id: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
 });
+
+const newBook = {
+  author_id: 1101,
+  title: "The Book Who Lived with the Dead",
+  author: "Kate Ellis"
+};
+
+const newAuthor = {
+  author_id: 1101,
+  name: "Kate Ellis"
+};
+
+connection
+  .sync({ force: true })
+  .then(() => {
+    return Book.create(newBook);
+  })
+  .then(() => {
+    return Author.create(newAuthor);
+  })
+  .catch(error => {
+    return console.log(error);
+  });
